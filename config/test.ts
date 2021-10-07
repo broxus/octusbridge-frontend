d1dc8c0d0445f522a4f3a0c50340d318d26400f7
Stepan Kamentsev <i.am.respawn@gmail.com>
2021-10-08 01:51:14 +0300

    Add CI and configs

---
+++ config/test.ts
@@ -0,0 +1,36 @@
+import { NetworkShape } from '@/bridge'
+
+
+export const networks: NetworkShape[] = [
+    {
+        chainId: '3',
+        currencySymbol: 'ETH',
+        explorerBaseUrl: 'https://ropsten.etherscan.io/',
+        label: 'Ropsten',
+        name: 'Ropsten',
+        rpcUrl: 'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161/',
+        type: 'evm',
+    },
+    {
+        chainId: '5',
+        currencySymbol: 'ETH',
+        explorerBaseUrl: 'https://goerli.etherscan.io/',
+        label: 'Goerli',
+        name: 'Goerli',
+        rpcUrl: 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161/',
+        type: 'evm',
+    },
+    {
+        chainId: '1',
+        currencySymbol: 'ETH',
+        explorerBaseUrl: 'https://tonscan.io/',
+        label: 'Free TON',
+        name: 'Free TON',
+        rpcUrl: '',
+        type: 'ton',
+    },
+]
+
+export const TokenAssetsURI = 'https://raw.githubusercontent.com/broxus/bridge-assets/master/test.json'
+
+export const TokenListURI = 'https://raw.githubusercontent.com/broxus/ton-assets/master/test.json'
