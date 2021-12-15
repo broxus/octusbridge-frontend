42051203774aa20a6ecee313424db1119de7965e
Denis Klimov <plitnichenko@gmail.com>
2021-12-15 18:42:08 +0300

    Supposed fix for EVM connection

--- src/stores/EvmWalletService.ts
+++ src/stores/EvmWalletService.ts
@@ -257,6 +257,8 @@ export class EvmWalletService {
                 this.data.address = address
                 this.state.isConnecting = false
                 this.state.isConnected = address !== undefined
+                this.state.isInitialized = true
+                this.state.isInitializing = true
             })
             log(`Sync EVM address: ${address}`)
         }
@@ -337,13 +339,14 @@ export class EvmWalletService {
 
         try {
             await this.syncChainId()
-            await this.syncAccountData()
-            await this.syncBalance()
 
             runInAction(() => {
                 this.state.isInitialized = this.cachedProvider === 'injected'
                 this.state.isInitializing = false
             })
+
+            await this.syncAccountData()
+            await this.syncBalance()
         }
         catch (e) {
             error('Fetch account data error', e)
