8af61ad3ff2498341cc7a296f905380159bf4a8e
Stepan Kamentsev <i.am.respawn@gmail.com>
2022-02-15 16:08:48 +0300

    Update config

--- config/prod.ts
+++ config/prod.ts
@@ -5,19 +5,52 @@ import { NetworkShape } from '@/types'
 
 export const networks: NetworkShape[] = [
     {
-        chainId: '5',
+        chainId: '1',
         currencySymbol: 'ETH',
-        explorerBaseUrl: 'https://goerli.etherscan.io/',
-        id: 'evm-5',
-        label: 'Goerli',
-        name: 'Ethereum Testnet Goerli',
-        rpcUrl: 'https://goerli.infura.io/v3/f3ca4333bf4a41308d0a277ae1c09336',
+        explorerBaseUrl: 'https://etherscan.io/',
+        id: 'evm-1',
+        label: 'Ethereum',
+        name: 'Ethereum Mainnet',
+        rpcUrl: 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
         transactionType: '0x2',
         type: 'evm',
     },
     {
+        chainId: '56',
+        currencySymbol: 'BNB',
+        explorerBaseUrl: 'https://bscscan.com/',
+        id: 'evm-56',
+        label: 'Binance Smart Chain',
+        name: 'BSC',
+        rpcUrl: 'https://bsc-dataseed.binance.org/',
+        transactionType: '0x0',
+        type: 'evm',
+    },
+    {
+        chainId: '250',
+        currencySymbol: 'FTM',
+        explorerBaseUrl: 'https://ftmscan.com/',
+        id: 'evm-250',
+        label: 'Fantom Opera',
+        name: 'Fantom Opera',
+        rpcUrl: 'https://rpc.ftm.tools/',
+        transactionType: '0x0',
+        type: 'evm',
+    },
+    {
+        chainId: '137',
+        currencySymbol: 'MATIC',
+        explorerBaseUrl: 'https://polygonscan.com/',
+        id: 'evm-137',
+        label: 'Polygon',
+        name: 'Polygon',
+        rpcUrl: 'https://matic-mainnet.chainstacklabs.com/',
+        transactionType: '0x0',
+        type: 'evm',
+    },
+    {
         chainId: '1',
-        currencySymbol: 'TON',
+        currencySymbol: 'EVER',
         explorerBaseUrl: 'https://tonscan.io/',
         id: 'ton-1',
         label: 'Everscale',
@@ -26,6 +59,7 @@ export const networks: NetworkShape[] = [
         type: 'ton',
     },
 ]
+
 
 export const AirdropContractAddress = new AddressLiteral('0:b321058503889a78fe0954cfb67564b218cff1eac1467dc48e4c80118dd6719f')
 
@@ -55,18 +89,16 @@ export const HiddenBridgeStrategyFactory = new AddressLiteral('0:0a6655e74d9f92c
 
 export const RelayEvmNetworkChainId = '1'
 
-export const StakingAccountAddress = new AddressLiteral('0:4465015903ea512bea517492aa5036620505d26e0ef6942f446e2df4e038ee49')
+export const StakingAccountAddress = new AddressLiteral('0:ec6a2fd6c3732e494684d016f1addec1a1828b6b7ecfcd30b34e8e5ad2d421d0')
 
-export const DaoRootContractAddress = new AddressLiteral('0:3c7a7ea44b60ea62822c40ecd6d2766b938180f63fc28da9c4f6dc89dc034199')
+export const DaoRootContractAddress = new AddressLiteral('0:6da8defd136c0227dfa24edde79728142d2bfb74bc9edf66ad86ae7cd8e56a86')
 
-export const TokenAssetsURI = 'https://raw.githubusercontent.com/broxus/bridge-assets/migration-1/test.json'
+export const TokenAssetsURI = 'https://raw.githubusercontent.com/broxus/bridge-assets/migration-1/main.json'
 
-export const TokenListURI = 'https://raw.githubusercontent.com/broxus/ton-assets/migration-1/test.json'
+export const TokenListURI = 'https://raw.githubusercontent.com/broxus/ton-assets/migration-1/manifest.json'
 
 export const Web3Url = 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'
 
 export const IndexerApiBaseUrl = 'https://bridge-indexer.broxus.com/v1'
 
 export const DaoIndexerApiBaseUrl = 'https://bridge-dao-indexer.broxus.com/v1'
-
-export const TonSwapIndexerApiBaseUrl = 'https://ton-swap-indexer.broxus.com/v1'
