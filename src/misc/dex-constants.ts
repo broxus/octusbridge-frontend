a0bdd32a586df76d35764a22b23d1a18e0de503f
Stepan Kamentsev <i.am.respawn@gmail.com>
2022-02-16 00:50:20 +0300

    Update dex root and wever address

--- src/misc/dex-constants.ts
+++ src/misc/dex-constants.ts
@@ -2,9 +2,9 @@ import { AddressLiteral } from 'everscale-inpage-provider'
 
 export class DexConstants {
 
-    static DexRootAddress = new AddressLiteral('0:943bad2e74894aa28ae8ddbe673be09a0f3818fd170d12b4ea8ef1ea8051e940');
+    static DexRootAddress = new AddressLiteral('0:5eb5713ea9b4a0f3a13bc91b282cde809636eb1e68d2fcb6427b9ad78a5a9008');
 
-    static WTONRootAddress = new AddressLiteral('0:0ee39330eddb680ce731cd6a443c71d9069db06d149a9bec9569d1eb8d04eb37');
+    static WTONRootAddress = new AddressLiteral('0:a49cd4e158a9a15555e624759e2e4e766d22600b7800d891e46f9291f044a93d');
 
     static TONDecimals = 9
 
