30cf9d1b7f8d3a6ce490bd18f7ed4dd26ae25faf
Denis Klimov <plitnichenko@gmail.com>
2022-03-15 19:47:27 +0100

    Change token assets manifest link

--- config/prod.ts
+++ config/prod.ts
@@ -97,7 +97,7 @@ export const StakingAccountAddress = new AddressLiteral('0:ec6a2fd6c3732e494684d
 
 export const DaoRootContractAddress = new AddressLiteral('0:6da8defd136c0227dfa24edde79728142d2bfb74bc9edf66ad86ae7cd8e56a86')
 
-export const TokenAssetsURI = 'https://raw.githubusercontent.com/broxus/bridge-assets/master/main.json'
+export const TokenAssetsURI = 'https://raw.githubusercontent.com/broxus/bridge-assets/everscale-based-1/main.json'
 
 export const TokenListURI = 'https://raw.githubusercontent.com/broxus/ton-assets/master/manifest.json'
 
