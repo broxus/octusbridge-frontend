1ce529820c222600b19deb5a5635d514a1729765
Denis Klimov <plitnichenko@gmail.com>
2022-07-21 02:33:38 +0800

    Increase pending withdrawal tries and timeout

--- src/modules/Bridge/stores/EverscaleToEvmPipeline.ts
+++ src/modules/Bridge/stores/EverscaleToEvmPipeline.ts
@@ -939,7 +939,7 @@ export class EverscaleToEvmPipeline extends BaseStore<EverscaleTransferStoreData
             await this.syncPendingWithdrawal()
 
             const isClosed = this.pendingWithdrawalStatus === 'Close'
-                || (this.pendingWithdrawalId === undefined && tries === 3)
+                || (this.pendingWithdrawalId === undefined && tries === 5)
 
             this.setState('releaseState', {
                 ...this.releaseState,
@@ -950,7 +950,7 @@ export class EverscaleToEvmPipeline extends BaseStore<EverscaleTransferStoreData
             if (!isClosed || this.pendingWithdrawalStatus === 'Open') {
                 this.pendingWithdrawalUpdater = setTimeout(async () => {
                     await runPendingWithdrawalUpdater()
-                }, 4000)
+                }, 5000)
             }
         }
 
