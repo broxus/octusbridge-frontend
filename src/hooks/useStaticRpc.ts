ac0c68b523372e8e65f1da3a39f3302552ba3961
Denis Klimov <plitnichenko@gmail.com>
2023-10-25 14:26:30 +0300

    Hotfix: standalone client connection

--- src/hooks/useStaticRpc.ts
+++ src/hooks/useStaticRpc.ts
@@ -1,20 +1,8 @@
 import { ProviderRpcClient } from 'everscale-inpage-provider'
-import { type ConnectionProperties, EverscaleStandaloneClient, checkConnection } from 'everscale-standalone-client'
-
-const MAINNET_JRPC: ConnectionProperties = 'mainnetJrpc'
-const MAINNET_GQL: ConnectionProperties = {
-    data: {
-        endpoints: ['https://mainnet.evercloud.dev/89a3b8f46a484f2ea3bdd364ddaee3a3/graphql'],
-    },
-    id: 42,
-    type: 'graphql',
-}
+import { EverscaleStandaloneClient } from 'everscale-standalone-client'
 
 const rpc = new ProviderRpcClient({
-    fallback: () => checkConnection('mainnetJrpc')
-        .then(() => MAINNET_JRPC)
-        .catch(() => MAINNET_GQL)
-        .then(connection => EverscaleStandaloneClient.create({ connection })),
+    fallback: () => EverscaleStandaloneClient.create({ connection: 'mainnetJrpc' }),
     forceUseFallback: true,
 })
 
